edf157c2eef3badf39513a4b7cde25334443dac8
Andrew <andrewmounir@outlook.com>
2023-03-07 12:04:27 +0200

    second commit

---
+++ LeetCode/FindNumbersWithEvenNumberOfDigits.js
@@ -0,0 +1,27 @@
+// Given an array nums of integers, return how many of them contain an even number of digits.
+// Input: nums = [12,345,2,6,7896]
+// Output: 2
+// Explanation:
+// 12 contains 2 digits (even number of digits).
+// 345 contains 3 digits (odd number of digits).
+// 2 contains 1 digit (odd number of digits).
+// 6 contains 1 digit (odd number of digits).
+// 7896 contains 4 digits (even number of digits).
+// Therefore only 12 and 7896 contain an even number of digits.
+
+var nums = [555, 901, 482, 1771];
+var findNumbers = function (nums) {
+  this.nums = nums;
+
+  var count = 0;
+
+  nums.map((num) => {
+    if (num.toString().length % 2 === 0) {
+      count++;
+    }
+  });
+  return count;
+};
+
+var a = findNumbers(nums);
+console.log(a);
